6f3a8afaeba00d6dfd61ad80840219db80004b33
RafaelKanda <rafaelmoutinhokanda@gmail.com>
2025-05-13 10:47:10 -0400

    Ajuste no front-end

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,11 +1,9 @@
 import axios from "axios";
-import dotenv from "dotenv";
 
-const { NODE_ENV } = dotenv.config().parsed;
+const VITE_NODE_ENV = import.meta.env.VITE_NODE_ENV;
 
-const URL = NODE_ENV === "development" ? "http://127.0.0.1:3000/api" : "/api";
-
-// const URL = "https://deploy-jornada-full-stack-qlr9.onrender.com/api";
+const URL =
+  VITE_NODE_ENV === "development" ? "http://127.0.0.1:3000/api" : "/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
