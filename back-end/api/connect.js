2f1ca760d5b2cceba25054f1dcd98975b9c5c8d3
RafaelKanda <rafaelmoutinhokanda@gmail.com>
2025-05-13 13:07:56 -0400

    Mais ajustes

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -1,15 +1,15 @@
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config({ path: path.resolve("./back-end/.env") });
-}
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const { DB_URI } = process.env.DB_URI;
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+console.log("DB_URI:", process.env.DB_URI);
 
-console.log(`DB_URI: ${DB_URI}`);
-
-const client = new MongoClient(DB_URI);
+const client = new MongoClient(process.env.DB_URI);
 
 export const db = client.db("spotify_clone");
