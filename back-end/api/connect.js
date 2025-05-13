2126e94e7ffcd745ff8042e95238e9a02e06b69c
RafaelKanda <rafaelmoutinhokanda@gmail.com>
2025-05-13 11:31:24 -0400

    Mais ajustes

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -1,5 +1,12 @@
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import path from "path";
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: path.resolve("./back-end/.env") });
+}
+
+console.log("DB_URI:", process.env.DB_URI);
 
 const { DB_URI } = dotenv.config().parsed ?? process.env;
 
